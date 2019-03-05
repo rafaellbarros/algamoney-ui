72fbf658a867e25cdfbfd44215cbf4f4973e89ae
Rafael Barros <rafaelbarros.df@gmail.com>
2019-03-05 00:34:45 -0300

    :fire: Removendo JwtModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,20 +2,15 @@ import { CommonModule, registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import localePt from '@angular/common/locales/pt';
 import { LOCALE_ID, NgModule } from '@angular/core';
-import { JwtModule } from '@auth0/angular-jwt';
 import { ToastyModule } from 'ng2-toasty';
 import { ConfirmationService } from 'primeng/components/common/api';
 import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
 import { SharedModule } from '../shared/shared.module';
 import { NaoAutorizadoComponent, PaginaNaoEncontradaComponent } from './components/inlines';
 import { NavbarComponent } from './components/navbar';
-
+import { RouterModule } from '@angular/router';
 
 registerLocaleData(localePt);
-
-export function tokenGetter() {
-  return localStorage.getItem('token');
-}
 
 @NgModule({
   declarations: [NavbarComponent, PaginaNaoEncontradaComponent, NaoAutorizadoComponent],
@@ -23,15 +18,8 @@ export function tokenGetter() {
     CommonModule,
     ToastyModule.forRoot(),
     ConfirmDialogModule,
-    SharedModule,
-    HttpClientModule,
-    JwtModule.forRoot({
-      config: {
-        tokenGetter: tokenGetter,
-        whitelistedDomains: ['localhost:8080'],
-        blacklistedRoutes: ['http://localhost:8080/oauth/token'],
-      }
-    })
+    RouterModule,
+    HttpClientModule
   ],
   exports: [
     NavbarComponent,
